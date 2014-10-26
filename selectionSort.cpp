96e12d2d6722b442d349afbba6ff111c80c01cfa
Anish Acharya <anisha@uci.edu>
2014-10-26 16:51:06 -0700

    Update selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,3 +1,7 @@
+//The good thing about selection sort is 
+//it never makes more than O(n) swaps and can be useful when memory write is a costly operation. 
+
+
 #include<iostream>
 
 using namespace std;
