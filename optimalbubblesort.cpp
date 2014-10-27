f1899b02a2dec8a08a490e21288bda9bc4858792
Anish Acharya <anisha@uci.edu>
2014-10-26 18:01:43 -0700

    Update optimalbubblesort.cpp

--- optimalbubblesort.cpp
+++ optimalbubblesort.cpp
@@ -1,4 +1,11 @@
 //min O(n), worst O(n^2)
+
+//Due to its simplicity, bubble sort is often used to introduce the concept of a sorting algorithm.
+//In computer graphics it is popular for its capability to detect a very small error 
+//(like swap of just two elements) in almost-sorted arrays and fix it with just linear complexity (2n). 
+//For example, it is used in a polygon filling algorithm, where bounding lines are sorted by their x coordinate 
+//at a specific scan line (a line parallel to x axis) and with incrementing y 
+//their order changes (two elements are swapped) only at intersections of two lines (Source: Wikipedia)
 #include<iostream>
 
 using namespace std;
