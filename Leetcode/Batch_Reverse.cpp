3c7dd25d6c21f65ad0a21124ab23154f1246f56e
debatrimitra02 <debatrim@uci.edu>
2016-04-25 22:34:19 -0700

    updated question

--- Leetcode/Batch_Reverse.cpp
+++ Leetcode/Batch_Reverse.cpp
@@ -1,3 +1,12 @@
+/*
+Given a array of integer and group size, reverse array by group size, example as follows: 
+[1, 2, 3, 4, 5, 6], 1 -> [1, 2, 3, 4, 5, 6] 
+[1, 2, 3, 4, 5, 6], 2 -> [2, 1, 4, 3, 6, 5] 
+[1, 2, 3, 4, 5, 6], 3 -> [3, 2, 1, 6, 5, 4] 
+[1, 2, 3, 4, 5, 6, 7, 8], 3 -> [3, 2, 1, 6, 5, 4, 8, 7] 
+Design test cases for you API
+*/
+
 #include <iostream>
 #include <vector>
 
