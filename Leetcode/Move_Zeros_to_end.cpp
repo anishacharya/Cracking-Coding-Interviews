62b38ebf9c0a7a02a04cdef6b4a541d082bdc5cc
Anish Acharya <aniacharya@ebay.com>
2016-05-16 00:24:35 -0700

    Update

---
+++ Leetcode/Move_Zeros_to_end.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums)
+    {
+        int count = 0;
+        // Count of non-zero elements
+        int n=nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] != 0)
+        {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    while (count < n)
+        nums[count++] = 0;
+    }
+};
