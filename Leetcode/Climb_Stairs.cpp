c0855eb4cf37c60062a6870874c203b30b7c6a43
Anish Acharya <aniacharya@ebay.com>
2016-05-16 01:03:39 -0700

    update

---
+++ Leetcode/Climb_Stairs.cpp
@@ -0,0 +1,15 @@
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
+class Solution {
+public:
+    int climbStairs(int n) 
+    {
+         vector<int> steps(n,0);
+         steps[0]=1; steps[1]=2;
+         for(int i=2;i<n;i++)
+             steps[i]=steps[i-2]+steps[i-1];
+         return steps[n-1];  
+    }
+};
