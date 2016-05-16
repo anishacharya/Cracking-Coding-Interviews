cc47be8ca16040a0377904ae1570142e919464ab
Anish Acharya <aniacharya@ebay.com>
2016-05-15 22:16:14 -0700

    updates

---
+++ Leetcode/Remove_Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+ int removeElement(vector<int>& nums, int val) 
+{
+    int point = 0;
+    for(int i = 0; i < nums.size(); i++) 
+    {
+        if(nums[i] != val)
+        {
+            nums[point] = nums[i];
+            point++;
+        }
+    }
+    return point;
+}
+};
