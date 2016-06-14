24e6fa711e07701e2b00f9694907dc52b7d19632
Anish Acharya <aniacharya@ebay.com>
2016-06-13 22:22:06 -0700

    Update Check BST or not

---
+++ Leetcode/isValidBST.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ /*
+ Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+Example 1:
+    2
+   / \
+  1   3
+Binary tree [2,1,3], return true.
+Example 2:
+    1
+   / \
+  2   3
+Binary tree [1,2,3], return false.
+*/
+
+class Solution {
+public:
+    TreeNode* prev = NULL;
+    bool isValidBST(TreeNode* root) 
+    {
+        if (root)
+        {
+            if (!isValidBST(root->left))
+                return false;
+            if (prev && root->val <= prev->val)
+                return false;
+            prev = root; cout<<prev->val<<endl;
+            return isValidBST(root->right);
+        }
+ 
+    return true;    
+    }
+};
