6aff4bb15645947d51575cde17307be944afb4c2
Anish Acharya <anisha@uci.edu>
2016-07-16 02:07:52 -0700

    Updating Design #1

--- Design/Mineswiper.cpp
+++ Design/Mineswiper.cpp
@@ -9,6 +9,7 @@ class Node
     public:
         bool visited;
         bool mine;	
+        int num;
 };
 
 // Define the Board
@@ -25,6 +26,7 @@ vector<vector<Node> > Create_Board(int m,int n)
 	{
             tmp[j].visited = false; 
             tmp[j].mine = false;
+            tmp[j].num = 0;
         }
         Board[i] = tmp;
         tmp.clear();
@@ -60,6 +62,40 @@ void place_mines(vector<vector<Node> >&Board, int &N)
     return place_mines(Board,N);	
 }
 
+int check(vector<vector<Node> >&Board,int i, int j)
+{
+    if(i >= 0 && i<Board.size() && j >= 0 && j < Board[0].size())
+    {
+        if(Board[i][j].mine == true)
+	    return 1;
+    }
+    return 0;
+}
+
+int get_num(vector<vector<Node> >&Board,int i, int j)
+{
+    int num = 0;
+    for(int p=i-1; p<i+1;p++)
+    {
+        for(int q = j-1; q < j+1; q++)
+            num += check(Board,p,q);	   
+    }
+    return num;
+}
+void set_number(vector<vector<Node> >&Board)
+{
+    if(Board.size() == 0)
+        return;
+    for(int i = 0; i< Board.size(); i++)
+    {
+        for(int j=0; j < Board[i].size(); j++)
+        {
+            if(Board[i][j].mine == false)
+                Board[i][j].num = get_num(Board,i,j);        
+        } 
+    }
+
+}
 
 int main()
 {
